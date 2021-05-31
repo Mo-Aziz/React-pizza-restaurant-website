2910a7c42b17c8b3f40d80804e5f39a723532891
MJ <mohamed.aref32@gmail.com>
2021-05-30 23:05:56 -0500

    creating toggle icon and sidebar

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React,{useState} from "react";
 import Navbar from "../Navbar";
+import Sidebar from "../Sidebar";
 import {
   HeroContainer,
   HeroContent,
@@ -10,12 +11,21 @@ import {
 } from "./HeroElments";
 
 const Hero = () => {
+
+  const [isOpen, setIsOpen]= useState(false); 
+
+  const toggle = () =>{
+
+    setIsOpen(!isOpen);
+  }
+  
   return (
     <HeroContainer>
-      <Navbar />
+      <Navbar toggle={toggle} />
+      <Sidebar  isOpen={isOpen} toggle={toggle}/>
       <HeroContent>
         <HeroItems>
-          <HeroH1> The greatest Pizza you can taste</HeroH1>
+          <HeroH1> The greatest Pizza you can Buy</HeroH1>
           <HeroP> Ready in 10 minutes</HeroP>
           <HeroBtn>Place Order</HeroBtn>
         </HeroItems>
